1b420ac8d886f7d3ac3c63d5c9e55dc0ffdc278d
wodencafe <wodencafe@gmail.com>
2023-08-06 14:57:49 -0500

    Refactor

--- lib/src/main/kotlin/chat/llamas/cardgame/Cards.kt
+++
@@ -1,7 +0,0 @@
-package chat.llamas.cardgame
-
-abstract class BaseCard {
-    abstract  val cardType: CardTypes
-    abstract  val unitType: UnitType
-    abstract  val regionType: Regions
-}
---
+++ lib/src/main/kotlin/chat/llamas/cardgame/weather/Climate.kt
@@ -0,0 +1,9 @@
+package chat.llamas.cardgame.weather
+
+enum class Climate {
+    A,  // Tropical
+    B,  // Dry
+    C,  // Temperate
+    D,  // Continental
+    E   // Polar
+}
